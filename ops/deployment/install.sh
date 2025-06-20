e02a7fe0642fbd9106905b0706e4e0b7495d0f03
Harsh Shah <shahharsh.210995@gmail.com>
2025-06-20 11:15:44 +0530

    Project init

---
+++ ops/deployment/install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+curl -fsSL https://deb.nodesource.com/setup_20.x | sudo -E bash -
+sudo apt install -y nodejs
+node -v
---
+++ ops/deployment/run.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+cd /mnt/app
+npm install --production
+npm start
